4425160ea0a56a84fda02d509aa9613af75e7b04
Chakhsu Lau <zexu.liu@live.cn>
2017-02-16 02:00:37 +0800

    🎊🎉first publish

--- footer.php
+++ footer.php
@@ -234,8 +234,10 @@ addCommentInputValue();
 </script>
 <?php endif; ?>
 <?php endif; ?>
-
 <script src="//cdn.bootcss.com/headroom/0.9.1/headroom.min.js"></script>
+<?php if ($this->options->useHighline == 'able'): ?>
+<script src="//cdn.bootcss.com/highlight.js/9.9.0/highlight.min.js"></script>
+<?php endif; ?>
 <?php if ($this->options->pjaxSet == 'able'): ?>
 <script src="//cdn.bootcss.com/instantclick/3.0.1/instantclick.min.js"></script>
 <?php endif; ?>
@@ -273,9 +275,6 @@ if ('addEventListener' in document) {
 }
 <?php endif; ?>
 </script>
-<?php if ($this->options->useHighline == 'able'): ?>
-<script src="//cdn.bootcss.com/highlight.js/9.9.0/highlight.min.js"></script>
-<?php endif; ?>
 <?php if ($this->options->useMathjax == 'able'): ?>
 <script type="text/x-mathjax-config">
 MathJax.Hub.Config({
