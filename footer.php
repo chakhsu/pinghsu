67ad80314dc6f1dc0dee6b5653b3e534f74f1a80
Chakhsu Lau <zexu.liu@live.cn>
2017-02-22 00:17:51 +0800

    remove something useless

--- footer.php
+++ footer.php
@@ -32,7 +32,6 @@
                         <?php endif; ?>
                     </a>
 					<div class="info-text">
-                        <p>Better late than never.</p>
                     	<p>Theme is <a href="https://github.com/chakhsu/pinghsu" target="_blank">Pinghsu</a> by <a href="https://www.linpx.com/" target="_blank">Chakhsu</a></p>
 						<p>Powered by <a href="http://www.typecho.org" target="_blank" rel="nofollow">Typecho</a></p>
 						<p>&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a></p>
