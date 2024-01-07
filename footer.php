addb4a13547849bc893ed224ca3c4748b6366549
Chakhsu.Lau <zexu.liu@live.cn>
2024-01-07 21:36:58 +0800

    feat: use tocbot

--- footer.php
+++ footer.php
@@ -53,97 +53,22 @@
 
 <?php if (($this->options->tableOfContents == 'able') && ($this->is('post'))): ?>
 <div id="directory-content" class="directory-content">
-    <div id="directory"></div>
+    <nav class="toc js-toc"></nav>
 </div>
+<script src="//cdn.bootcdn.net/ajax/libs/tocbot/4.21.0/tocbot.min.js"></script>
 <script>
-var postDirectoryBuild = function() {
-    var postChildren = function children(childNodes, reg) {
-        var result = [],
-            isReg = typeof reg === 'object',
-            isStr = typeof reg === 'string',
-            node, i, len;
-        for (i = 0, len = childNodes.length; i < len; i++) {
-            node = childNodes[i];
-            if ((node.nodeType === 1 || node.nodeType === 9) &&
-                (!reg ||
-                isReg && reg.test(node.tagName.toLowerCase()) ||
-                isStr && node.tagName.toLowerCase() === reg)) {
-                result.push(node);
-            }
-        }
-        return result;
-    },
-    createPostDirectory = function(article, directory, isDirNum) {
-        var contentArr = [],
-            titleId = [],
-            levelArr, root, level,
-            currentList, list, li, link, i, len;
-        levelArr = (function(article, contentArr, titleId) {
-            var titleElem = postChildren(article.childNodes, /^h\d$/),
-                levelArr = [],
-                lastNum = 1,
-                lastRevNum = 1,
-                count = 0,
-                guid = 1,
-                id = 'directory' + (Math.random() + '').replace(/\D/, ''),
-                lastRevNum, num, elem;
-            while (titleElem.length) {
-                elem = titleElem.shift();
-                contentArr.push(elem.innerHTML);
-                num = +elem.tagName.match(/\d/)[0];
-                if (num > lastNum) {
-                    levelArr.push(1);
-                    lastRevNum += 1;
-                } else if (num === lastRevNum ||
-                    num > lastRevNum && num <= lastNum) {
-                    levelArr.push(0);
-                    lastRevNum = lastRevNum;
-                } else if (num < lastRevNum) {
-                    levelArr.push(num - lastRevNum);
-                    lastRevNum = num;
-                }
-                count += levelArr[levelArr.length - 1];
-                lastNum = num;
-                elem.id = elem.id || (id + guid++);
-                titleId.push(elem.id);
-            }
-            if (count !== 0 && levelArr[0] === 1) levelArr[0] = 0;
+function addTocId() {
+    var id = 1;
+    var entryContent = document.querySelector('.post-content');
+    var headings = entryContent.querySelectorAll('h1, h2, h3, h4, h5');
 
-            return levelArr;
-        })(article, contentArr, titleId);
-        currentList = root = document.createElement('ul');
-        dirNum = [0];
-        for (i = 0, len = levelArr.length; i < len; i++) {
-            level = levelArr[i];
-            if (level === 1) {
-                list = document.createElement('ul');
-                if (!currentList.lastElementChild) {
-                    currentList.appendChild(document.createElement('li'));
-                }
-                currentList.lastElementChild.appendChild(list);
-                currentList = list;
-                dirNum.push(0);
-            } else if (level < 0) {
-                level *= 2;
-                while (level++) {
-                    if (level % 2) dirNum.pop();
-                    currentList = currentList.parentNode;
-                }
-            }
-            dirNum[dirNum.length - 1]++;
-            li = document.createElement('li');
-            link = document.createElement('a');
-            link.href = '#' + titleId[i];
-            link.innerHTML = !isDirNum ? contentArr[i] :
-                dirNum.join('.') + ' ' + contentArr[i] ;
-            li.appendChild(link);
-            currentList.appendChild(li);
-        }
-        directory.appendChild(root);
-    };
-    createPostDirectory(document.getElementById('post-content'),document.getElementById('directory'), true);
-};
-postDirectoryBuild();
+    headings.forEach(function(heading) {
+        var hyphenated = 'anchor-' + id;
+        heading.setAttribute('id', hyphenated);
+        id++;
+    });
+}
+addTocId();
 </script>
 <?php endif; ?>
 <?php if(($this->is('single')) && ($this->allow('comment'))): ?>
@@ -248,6 +173,16 @@ var postDirectory = new Headroom(document.getElementById("directory-content"), {
     }
 });
 postDirectory.init();
+tocbot.init({
+    tocSelector: '.toc',
+    contentSelector: '.post-content',
+    headingSelector: 'h1, h2, h3, h4, h5',
+    collapseDepth: 2,
+    scrollSmooth: true,
+    scrollSmoothDuration: 600,
+    scrollSmoothOffset: -80,
+    headingsOffset: 80,
+});
 <?php endif; ?>
 <?php if($this->is('post')): ?>
 var postSharer = new Headroom(document.getElementById("post-bottom-bar"), {
